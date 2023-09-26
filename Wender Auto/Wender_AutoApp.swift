2c9022d266e5ff8b2003b5651a13d8a709b058e5
Wender <outrowender@gmail.com>
2023-09-26 11:45:46 -0300

    Initial Commit

---
+++ Wender Auto/Wender_AutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wender_AutoApp.swift
+//  Wender Auto
+//
+//  Created by Wender on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Wender_AutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
